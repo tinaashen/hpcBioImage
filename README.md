6226bfaef952dd0f33e020ec3a80a23f421f64a4
Tina Shen <shentina18@gmail.com>
2023-12-09 17:05:11 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+Python BioImage Analysis Tutorial
+=================================
+
+*originally created in 2016*    
+*updated and converted to a Jupyter notebook in 2017*    
+*updated and converted to python 3 in 2018*    
+*by Jonas Hartmann (Mayor group, University College London)*    
+
+----
+
+⚠️ Note that some of the materials in this tuorial are slightly out of date by now (2023), but recent feedback has shown that the tutorial is still very useful and approachable for many learners. I would like to create an updated and slightly extended version at some point, but academic pressures limit my ability to work on this, so we'll have to see how it goes... 🤞
+
+(Latest maintenance update: 17.02.2023, python 3.9.16 (basic Anaconda distro), with thanks to @Koushouu!)
+
+-----
+
+## Aims and Overview
+
+This tutorial teaches the basics of bio-image processing, segmentation and analysis in python. It integrates explanations and exercises in a (hopefully) self-explanatory fashion, enabling participants to build their own image analysis pipelines step by step.
+
+The tutorial uses single-cell segmentation of 2D confocal fluorescence microscopy images to illustrate key concepts from preprocessing to segmentation to (very basic) data analysis. It concludes with a small section on how to apply such a pipeline to multiple images at once (batch processing).
+
+Everything you need to know to get started can be found in the jupyter notebook `image_analysis_tutorial.ipynb`. To find out more about how to run these materials interactively, see the [Jupyter documentation](https://jupyter.readthedocs.io/en/latest/index.html).
+
+Note that this tutorial was part of a course aimed at people with basic knowledge of python. The course included introductory sessions/lectures on scientific python (in particular `numpy` and `matplotlib`) as well as on image analysis (see the slides in this repository). For those tackling this tutorial on their own, it is therefore recommended to first acquire basic scientific python knowledge elsewhere (e.g. at [python-course.eu](https://www.python-course.eu)).
+
+## Content Overview
+
+- Lecture
+  - Working with digital images
+    - Images as arrays of numbers
+    - Look-up tables (LUTs)
+    - Dimensions
+    - Bit-depth
+  - Image analysis pipelines
+    - Preprocessing: filters, kernels, convolution, background subtraction
+    - Foreground detection: thresholding, morphological operations
+    - Segmentation: labels, seeds, watershed
+    - Postprocessing: object filtering
+    - Making measurements
+
+- Tutorial
+  - Importing Modules & Packages
+  - Loading & Handling Image Data
+  - Preprocessing
+  - Manual Thresholding & Threshold Detection
+  - Adaptive Thresholding
+  - Improving Masks with Binary Morphology
+  - Connected Components Labeling
+  - Cell Segmentation by Seeding & Expansion
+  - Postprocessing: Removing Cells at the Image Border
+  - Identifying Cell Edges
+  - Extracting Quantitative Measurements
+  - Simple Analysis & Visualization
+  - Writing Output to Files
+  - Batch Processing
+
+## Old Versions and Other Sources
+
+This was part of the EMBL Bio-IT/ALMF `Image Analysis with Python 2018` course (see the [EMBL Gitlab repo](https://git.embl.de/grp-bio-it/image-analysis-with-python-2018)).
+
+If you are looking for the python 2 version from 2017, see the `2017_legacy_python_version` branch or the corresponding [EMBL GitLab repo](https://git.embl.de/grp-bio-it/python-workshop-image-processing).
+
+The original 2016 materials can be found in Karin Sasaki's corresponding Github [repo](https://github.com/karinsasaki/python-workshop-image-processing).
+
+## Acknowledgements
+
+The first version of this tutorial was created for the `EMBL Python Workshop - Image Processing` course organized by Karin Sasaki and Jonas Hartmann in 2016. Additional lecturers and TAs contributing to this course were Kota Miura, Volker Hilsenstein, Aliaksandr Halavatyi, Imre Gaspar, and Toby Hodges.
+
+The second installment (the `EMBL Bio-IT Image Processing Course`, 2017) was organized and taught by Jonas Hartmann and Toby Hodges.
+
+The third version of this tutorial was part of the `EMBL Bio-IT/ALMF Image Analysis with Python 2018` course, organized by Jonas Hartmann and Toby Hodges in collaboration with Tobias Rasse and Volker Hilsenstein. Additional organizational help came from Christian Tischer and Malvika Sharan.
+
+Many thanks to all the helpful collaborators and the interested students who were instrumental in making these courses a success.
+
+## Feedback
+
+Feedback on this tutorial is always welcome! Please open an issue on GitHub or write to *jonas.hartmann(AT)ucl.ac.uk*.
---
+++ batch_processing_solution.py
@@ -0,0 +1,160 @@
+
+# coding: utf-8
+
+# Image Analysis with Python - Solution for Batch Processing
+
+# The following is the script version of the tutorial's solution pipeline, where all the code
+# has been wrapped in a single function that can be called many times for many images.
+# Please refer to the jupyter notebooks ('image_analysis_tutorial[_solutions].ipynb') for 
+# more information, including detailed comments on every step.
+
+
+## Importing Modules & Packages
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.ndimage as ndi
+import sys
+import csv
+
+
+## Defining the pipeline function
+def run_pipeline():
+    """Run 2D single-cell segmentation pipeline optimized for membrane-labeled
+    spinning-disk confocal images of membrane markers in zebrafish early embryos.
+    
+    Parameters
+    ----------
+    dirpath : string
+        Path to the directory containing the input image.
+    filename : string
+        Name of the input file, including file ending (should be .tif).
+    
+    Returns
+    -------
+    clean_ws : 3D numpy array of same shape as input image
+        The single-cell segmentation. Every cell is labeled with a unique
+        integer ID. Background is 0.
+    results : dict
+        A number of measurements extracted from each cell. The dict keys 
+        name the type of measurement. The dict values are lists containing
+        the measured values. The order of all lists is the same and relates
+        to the segmentation IDs through the list in results['cell_id'].
+    """
+    dirpath = sys.argv[1]
+    
+    filename = sys.argv[2]
+    if dirpath != True or filename != True:
+        print("path not found")
+
+    ## Importing & Handling Image Data
+    print("hello")
+    from os.path import join
+    filepath = join(dirpath, filename)
+
+    from skimage.io import imread
+    img = imread(filepath)
+
+
+    ## Preprocessing
+
+    sigma = 3
+    img_smooth = ndi.gaussian_filter(img, sigma)
+
+
+    ## Adaptive Thresholding
+
+    i = 31
+    SE = (np.mgrid[:i,:i][0] - np.floor(i/2))**2 + (np.mgrid[:i,:i][1] - np.floor(i/2))**2 <= np.floor(i/2)**2
+
+    from skimage.filters import rank 
+    bg = rank.mean(img_smooth, footprint=SE)
+
+    mem = img_smooth > bg
+
+
+    ## Improving Masks with Binary Morphology
+
+    mem_holefilled = ~ndi.binary_fill_holes(~mem) # Short form
+
+    i = 15
+    SE = (np.mgrid[:i,:i][0] - np.floor(i/2))**2 + (np.mgrid[:i,:i][1] - np.floor(i/2))**2 <= np.floor(i/2)**2
+
+    pad_size = i+1
+    mem_padded = np.pad(mem_holefilled, pad_size, mode='reflect')
+    mem_final = ndi.binary_closing(mem_padded, structure=SE)
+    mem_final = mem_final[pad_size:-pad_size, pad_size:-pad_size]
+
+
+    ## Cell Segmentation by Seeding & Expansion
+
+    ### Seeding by Distance Transform
+
+    dist_trans = ndi.distance_transform_edt(~mem_final)
+    dist_trans_smooth = ndi.gaussian_filter(dist_trans, sigma=5)
+
+    from skimage.feature import peak_local_max
+    seed_coords = peak_local_max(dist_trans_smooth, min_distance=10)
+    seeds = np.zeros_like(dist_trans_smooth, dtype=bool)
+    seeds[tuple(seed_coords.T)] = True
+
+    seeds_labeled = ndi.label(seeds)[0]
+
+    ### Expansion by Watershed
+
+    from skimage.segmentation import watershed
+    ws = watershed(img_smooth, seeds_labeled)
+
+
+    ## Postprocessing: Removing Cells at the Image Border
+
+    border_mask = np.zeros(ws.shape, dtype=bool)
+    border_mask = ndi.binary_dilation(border_mask, border_value=1)
+
+    clean_ws = np.copy(ws)
+
+    for cell_ID in np.unique(ws):
+        cell_mask = ws==cell_ID
+        cell_border_overlap = np.logical_and(cell_mask, border_mask)
+        total_overlap_pixels = np.sum(cell_border_overlap)
+        if total_overlap_pixels > 0: 
+            clean_ws[cell_mask] = 0
+
+    for new_ID, cell_ID in enumerate(np.unique(clean_ws)[1:]): 
+        clean_ws[clean_ws==cell_ID] = new_ID+1
+
+
+    ## Identifying Cell Edges
+
+    edges = np.zeros_like(clean_ws)
+
+    for cell_ID in np.unique(clean_ws)[1:]:
+        cell_mask = clean_ws==cell_ID
+        eroded_cell_mask = ndi.binary_erosion(cell_mask, iterations=1)
+        edge_mask = np.logical_xor(cell_mask, eroded_cell_mask)
+        edges[edge_mask] = cell_ID
+
+
+    ## Extracting Quantitative Measurements
+
+    with open('results.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(['cell_id', 'int_mean', 'int_mem_mean', 'cell_area', 'cell_edge'])
+        
+        # Write the data rows
+        for cell_id in np.unique(clean_ws)[1:]:
+            cell_mask = clean_ws==cell_id
+            edge_mask = edges==cell_id
+            int_mean = np.mean(img[cell_mask])
+            int_mem_mean = np.mean(img[edge_mask])
+            cell_area = np.sum(cell_mask)
+            cell_edge = np.sum(edge_mask)
+            print('cell_id ' + str(cell_id) + ':\nint_mean = ' + str(int_mean) + '\nint_mem_mean = ' + str(int_mem_mean) + '\ncell_area = ' + str(cell_area) + '\ncell_edge = ' + str(cell_edge))
+            writer.writerow([cell_id, int_mean, int_mem_mean, cell_area, cell_edge])
+    ## Returning the results
+    return clean_ws
+
+
+if __name__ == "__main__":
+    run_pipeline()
